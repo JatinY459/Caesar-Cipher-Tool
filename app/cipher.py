4f37ab3329564083560b6d71cca8307202fefd0f
JatinY459 <14jatinyadav@gmail.com>
2025-04-30 21:54:38 +0530

    debugged encrypt & brute decrypt functions

--- app/cipher.py
+++ app/cipher.py
@@ -3,21 +3,25 @@ import string
 LAST_LETTER_CODE = 90 #last letter code for 'Z'
 ALPHABET_RANGE = 26 
 
+def shift_char(char, shift):
+    if char.isalpha():
+        shifted = ord(char) + shift
+        if shifted > LAST_LETTER_CODE:
+            # Coming back to start of alphabet when Z  gone ahead basically we circle back to A
+            shifted -= ALPHABET_RANGE
+        return chr(shifted)
+    return char
+
+
 def caesar_shift(text, shift):
     # Take input text/message and shift value
     result = [] # Result string
     # loop through character but operating only on uppercase letters
     # Ignoring all other characters & adding them to result as is
     for char in text.upper():
-        if char.isalpha():
-            shifted = ord(char) + shift
-            if shifted > LAST_LETTER_CODE:
-                # Coming back to start of alphabet when Z  gone ahead basically we circle back to A
-                shifted -= ALPHABET_RANGE
-            result.append(chr(shifted))
-        else:
-            result.append(char)
-    return str(result)
+        result.append(shift_char(char, shift))
+    
+    return "".join(result)
 
 
 def caesar_decrypt_brute(text):
@@ -25,8 +29,9 @@ def caesar_decrypt_brute(text):
     # Loop through all possible shifts i.e., 25 possible shift values
     for shift in range(1, ALPHABET_RANGE):
         # un-"caesar shifting" the text by shifting back by same number
-        possible_message = caesar_shift(text, -shift)
-        results.append(str(possible_message))
+        possible_message = caesar_shift(text, shift)
+        results.append(possible_message)
+        results.reverse() # Shift would be same as index+1 here
     return results
 
 
--- test.py
+++ test.py
@@ -3,6 +3,16 @@
 
 # print(list(common_words)[9])
 
+
+# ----- #----- #----- #----- #----- #----- #----- #----- #----- #----- #----- #----- #----- #
 import app.cipher as cipher
 
-print(cipher.caesar_decrypt_brute("ifmmp"))
+# print(cipher.caesar_decrypt_brute("ifmmp"))
+# ----- #----- #----- #----- #----- #----- #----- #----- #----- #----- #----- #----- #----- #
+LAST_LETTER_CODE = 90 #last letter code for 'Z'
+ALPHABET_RANGE = 26
+text = "ifmmp"
+shift = -1
+result = [] # Result string
+
+print(cipher.caesar_decrypt_auto(text))
